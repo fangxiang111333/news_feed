d5d156c8325a350634704a8ff8484966ec6c393c
xufang <1229570068@qq.com>
2017-11-21 20:49:41 +0800

    新民网

--- crawls/news_spider/spiders/newsspider.py
+++ crawls/news_spider/spiders/newsspider.py
@@ -5,7 +5,7 @@
 @date:   2017-08-23
 """
 
-from ..items import NewsItem
+from crawls.news_spider.items import NewsItem
 
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
@@ -307,6 +307,50 @@ class JxcbwNewsSpider(CrawlSpider):
         item['comments'] = comments
         yield item
 
+#新民网   successful
+class XinminNewsSpider(CrawlSpider):
+    name = "xinmin_news_spider"
+    allowed_domains = ['xinmin.cn']
+    start_urls = ['http://www.xinmin.cn']
+    #http://edu.xinmin.cn/tt/2017/11/15/31334031.html
+    #http://newsxmwb.xinmin.cn/world/2017/11/21/31335695.html
+    url_pattern = r'(http://.*\.xinmin\.cn)/.*/(\d{4})/(\d{2})/(\d{2})/(\d+).html'
+    rules = [
+            Rule(LxmlLinkExtractor(allow=[url_pattern]), callback='parse_news', follow=True)
+        ]
+
+    def parse_news(self, response):
+        sel = Selector(response)
+        pattern = re.match(self.url_pattern, str(response.url))
+        source = pattern.group(1)
+        date = pattern.group(2) + '/' + pattern.group(3) + '/' + pattern.group(4)
+        newsId = pattern.group(5)
+        if sel.xpath('//div[@class="info"]/span/text()'):
+            time = sel.xpath('//div[@class="info"]/span/text()').extract()[-1]
+        elif sel.xpath('//span[@class="page_time"]/text()'):
+            time = sel.xpath('//span[@class="page_time"]/text()').extract()
+        else:
+            time = "unknown"
+        if sel.xpath('//h1[@class="article_title"]/text()'):
+            title = sel.xpath('//h1[@class="article_title"]/text()').extract()
+        elif sel.xpath('//h3[@class="content_title"]/text()'):
+            title = sel.xpath('//h3[@class="content_title"]/text()').extract()
+        else:
+            title = "unknown"
+        url = response.url
+        contents = ListCombiner(sel.xpath('//div[@class="a_p"]/p/text()').extract())
+        comments = 0
+
+        item = NewsItem()
+        item['source'] = source
+        item['title'] = title
+        item['date'] = date
+        item['time'] = time
+        item['newsId'] = newsId
+        item['url'] = url
+        item['contents'] = contents
+        item['comments'] = comments
+        yield item
 
 
 # 央视新闻 successful
